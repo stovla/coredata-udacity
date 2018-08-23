02c10b9773202f07952707df6682de148314731a
Vlastimir Radojevic <vradojevic@intellimec.com>
2018-08-23 08:26:12 +0200

    updated tableView to use fetchedResultsController

--- Mooskine/Mooskine-starter/Mooskine/View Controllers/NotebooksListViewController.swift
+++ Mooskine/Mooskine-starter/Mooskine/View Controllers/NotebooksListViewController.swift
@@ -188,3 +188,26 @@ class NotebooksListViewController: UIViewController, UITableViewDataSource, NSFe
         }
     }
 }
+
+extension NotebooksListViewController: NSFetchedResultsControllerDelegate {
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .fade)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .fade)
+        default:
+            break
+        }
+    }
+}
+
